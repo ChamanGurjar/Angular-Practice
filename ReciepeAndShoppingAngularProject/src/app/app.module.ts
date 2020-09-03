9eb0a61d0a0062c03b46161d9bda64f7a22139d6
Chaman Gurjar <chamangujjar162@gmail.com>
2020-09-03 08:36:00 +0530

    Bug Fix

--- ReciepeAndShoppingAngularProject/src/app/app.module.ts
+++ ReciepeAndShoppingAngularProject/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ReciepesComponent } from './reciepes/reciepes.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
@@ -16,7 +15,6 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   declarations: [
     AppComponent,
     HeaderComponent,
-    ReciepesComponent,
     RecipesComponent,
     RecipeDetailComponent,
     RecipeListComponent,
