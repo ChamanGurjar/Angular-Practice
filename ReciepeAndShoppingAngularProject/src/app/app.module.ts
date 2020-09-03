ebf69e81164defdece395eaf7494740c46fa82cd
Chaman Gurjar <chamangujjar162@gmail.com>
2020-09-03 08:27:21 +0530

    Recipes component added

--- ReciepeAndShoppingAngularProject/src/app/app.module.ts
+++ ReciepeAndShoppingAngularProject/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ReciepesComponent } from './reciepes/reciepes.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ReciepesComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
