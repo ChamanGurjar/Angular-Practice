a6b1ae15fd7fc740689066e43ac68bc9da322ca6
Chaman Gurjar <chamangujjar162@gmail.com>
2020-09-10 08:44:23 +0530

    Form Created.

--- FormsInAngular/src/app/app.component.ts
+++ FormsInAngular/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'FormsInAngular';
+  projectForm: FormGroup;
+
+  ngOnInit(): void {
+    this.setupForm();
+  }
+
+  private setupForm() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('critical')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.valid);
+    console.log(this.projectForm);
+    console.log(this.projectForm.value);
+  }
+
 }
