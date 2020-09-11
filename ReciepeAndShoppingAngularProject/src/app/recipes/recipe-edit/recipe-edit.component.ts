a82012d6f020d7e8afe2936258b282d570c48c84
Chaman Gurjar <chamangujjar162@gmail.com>
2020-09-11 21:36:51 +0530

    IMP = FORM ARRAY IMPLEMENTATION. Recipe ingredients shown on the screen in edit mode.

--- ReciepeAndShoppingAngularProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ ReciepeAndShoppingAngularProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, NgForm, FormControl } from '@angular/forms';
+import { FormGroup, NgForm, FormControl, FormArray } from '@angular/forms';
 
 import { RecipeService } from 'src/app/service/recipe.service';
 
@@ -29,24 +29,42 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let imagePath = '';
     let description = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipeByIndex(this.id);
       recipeName = recipe.name
       imagePath = recipe.imagePath
       description = recipe.description
+
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
+
     }
 
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(imagePath),
-      'description': new FormControl(description)
+      'description': new FormControl(description),
+      'ingredients': recipeIngredients
     });
 
   }
 
   onSubmit() {
     console.log(this.recipeForm);
+  }
+
+  get controls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls
   }
 
 }
